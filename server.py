8655a18a6d4e1f1035def5e51ce8472a463b63d2
XiaoleZ <xiaole2@ualberta.ca>
2020-01-19 18:49:23 -0700

    added license and formatted the coding style

--- server.py
+++ server.py
@@ -1,16 +1,15 @@
-#  coding: utf-8 
-import socketserver
-import http.server
+#  coding: utf-8
 import os
+import socketserver
 
 # Copyright 2013 Abram Hindle, Eddie Antonio Santos
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -28,17 +27,32 @@ import os
 
 # try: curl -v -X GET http://127.0.0.1:8080/
 
-#current os path
-PATH = os.getcwd() + "/www/"
-INDEX = "index.html"
+
+# Copyright 2020 Xiaole Zeng
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# current os path
+INDEX = 'index.html'
+PATH = os.getcwd() + '/www/'
+
 
 class MyWebServer(socketserver.BaseRequestHandler):
-    
+
     def handle(self):
         self.data = self.request.recv(1024).strip()
-        print ("Got a request of: %s\n" % self.data)
-        #self.request.sendall(bytearray("OK",'utf-8'))
-      
+        print("Got a request of: %s\n" % self.data)
+
         if self.data != b'':
             request_method = str(self.data).split(' ')[0]
             request_path = str(self.data).split(' ')[1]
@@ -49,63 +63,69 @@ class MyWebServer(socketserver.BaseRequestHandler):
             else:
                 self.status_405()
 
-
-    def response(self,path):     
+    def response(self, path):
         try:
-            if  os.path.join(PATH, path).endswith('.html') or (os.path.isdir(os.path.join(PATH, path.strip('/'))) and path.endswith('/')):
-                
+            path_for_dir = os.path.join(PATH, path.strip('/'))
+
+            if os.path.join(PATH, path).endswith('.html') or (
+                os.path.isdir(path_for_dir)
+                and path.endswith('/')
+            ):
+
                 if os.path.isdir(os.path.join(PATH, path.strip('/'))):
-                    path = path + INDEX 
-                f = open( os.path.join(PATH, path[1:]), "r")
+                    path = path + INDEX
+                f = open(os.path.join(PATH, path[1:]), 'r')
                 data = f.read()
-                t = 'html'
+                mimetypes = 'html'
                 f.close()
-                self.status_200(t,data)
+                self.status_200(mimetypes, data)
             elif os.path.join(PATH, path).endswith('.css'):
                 try:
-                    f = open( os.path.join(PATH, path[1:]), "r")
+                    f = open(os.path.join(PATH, path[1:]), 'r')
                     data = f.read()
-                    t = 'css'
+                    mimetypes = 'css'
                     f.close()
-                    self.status_200(t,data)
-                except:
-                    self.status_404()  
-            elif os.path.isdir(os.path.join(PATH, path.strip('/'))) and not path.endswith('/'):
+                    self.status_200(mimetypes, data)
+                except Exception:
+                    self.status_404()
+            elif os.path.isdir(path_for_dir) and not path.endswith('/'):
                 self.status_301(path)
-            else:       
-                self.status_404()      
+            else:
+                self.status_404()
         except Exception as e:
             print(e)
-    
-    def status_200(self,mimetypes,data):
-        self.request.sendall(bytearray("HTTP/1.1 200 OK\r\n" + 
-            "Content-Type: text/"+mimetypes+ "\r\n\r\n"+
-            data , 'utf-8'))
 
-    def status_301(self,path):
-        print("HTTP/1.1 301 Move Permanently\r\nLocation: http://127.0.0.1:8080"+path +"/\r\nContent-Type: text/html\r\n")
-        self.request.sendall(bytearray("HTTP/1.1 301 Move Permanently\r\nRedirected to :"+ path +"/\r\nContent-Type: text/html\r\n\r\n"  , 'utf-8')) 
+    def status_200(self, mimetypes, data):
+        self.request.sendall(bytearray('HTTP/1.1 200 OK\r\n' +
+                                       'Content-Type: text/' + mimetypes +
+                                       '\r\n\r\n' +
+                                       data, 'utf-8'))
+
+    def status_301(self, path):
+        self.request.sendall(bytearray('HTTP/1.1 301 Move Permanently\r\n' +
+                                       'Redirected to :' + path + '/\r\n' +
+                                       'Content-Type: text/html\r\n\r\n',
+                                       'utf-8'))
 
     def status_404(self):
-        #print("here\n")
-        self.request.sendall(bytearray("HTTP/1.1 404 Not Found\r\n"
-         "Content-Type: text/html\r\n\r\n" +
-        "<body>\
-        <h1>Error response</h1> \
-        <p>Error code 404.</p> \
-        <p>Message: File not found.</p>\
-        </body> ", 'utf-8')) 
+        self.request.sendall(bytearray('HTTP/1.1 404 Not Found\r\n'
+                                       'Content-Type: text/html\r\n\r\n' +
+                                       '<body>'
+                                       '<h1>Error response</h1>'
+                                       '<p>Error code 404.</p>'
+                                       '<p>Message: File not found.</p>'
+                                       '</body> ', 'utf-8'))
 
     def status_405(self):
-        self.request.sendall(bytearray("HTTP/1.1 405 Method Not Allowed\r\n"
-         "Content-Type: text/html\r\n\r\n" +
-        "<body>\
-        <h1>Error response</h1> \
-        <p>Error code 405.</p> \
-        <p>Message: Method Not Allowed.</p>\
-        </body> ", 'utf-8')) 
+        self.request.sendall(bytearray('HTTP/1.1 405 Method Not Allowed\r\n'
+                                       'Content-Type: text/html\r\n\r\n' +
+                                       '<body>'
+                                       '<h1>Error response</h1>'
+                                       '<p>Error code 405.</p>'
+                                       '<p>Message: Method Not Allowed.</p>'
+                                       '</body> ', 'utf-8'))
 
-        
+
 if __name__ == "__main__":
     HOST, PORT = "localhost", 8080
 
